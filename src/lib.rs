e6c383f4afef155ee139d1c82e60c99dac84d809
bragaz <leo.braga95@gmail.com>
2020-09-23 11:15:34 +0200

    added case insensitve option with env var print output on file

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 use std::fs;
 use std::error::Error;
+use std::env;
 
 pub struct Config {
     pub query: String,
     pub filename: String,
+    pub case_sensitive: bool
 }
 
 impl Config {
@@ -12,10 +14,14 @@ impl Config {
         if args.len() < 3 {
             return Err("Not enough arguments");
         }
+
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
         // we started from one because the vector take as index 0 value the program's name
         return Ok(Config {
             query: args[1].clone(),
             filename: args[2].clone(),
+            case_sensitive,
         });
     }
 }
@@ -25,7 +31,13 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
-    for line in search(&config.query, &contents) {
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+
+    for line in results {
         println!("{}", line);
     }
 
@@ -35,25 +47,56 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 // search look up for query inside the given contents and returns a vector of
 // any repetition of query itself
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut res = Vec::new();
+    let mut result = Vec::new();
     for line in contents.lines() {
         if line.contains(query) {
-            res.push(line);
+            result.push(line);
         }
     }
-    res
+    result
 }
 
+// search_case_insensitive look up for query inside the given content and returns a vector of
+// any repetition of query. This one is case insensitive.
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut result = Vec::new();
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            result.push(line);
+        }
+    }
+    result
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "mani";
-        let contents = "ini, mini, mani, mo";
+        let contents = "\
+        ini, \
+        mini, \
+        mani, \
+        mo";
         assert_eq!(vec!["ini, mini, mani, mo"], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -11,14 +11,14 @@ fn main() {
     // unwrap_or_else read the Result<T,E> and return the type if there's some or the error
     let config = minigrep::Config::new_config(args.as_slice())
         .unwrap_or_else(|err| {
-        println!("{}", err);
+        eprintln!("{}", err);
         // this line stops the execution immediately
         process::exit(1);
     });
 
     // here we don't use unwrap_or_else cause we don't care about anything but the error
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
